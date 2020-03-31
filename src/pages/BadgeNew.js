631118e57cf4fcb57ecd18be667de39be8e11ba4
Paola Alapizco <pahoc.alapizco@gmail.com>
2020-03-31 10:37:42 -0700

    Badge added to initial page

--- src/pages/BadgeNew.js
+++ src/pages/BadgeNew.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Navbar from '../components/Navbar';
 import header from '../images/badge-header.svg';
+import Badge from './../components/Badge';
 
 import './styles/BadgeNew.css';
 
@@ -11,6 +12,18 @@ class BadgeNew extends React.Component {
         <Navbar />
         <div className="BadgeNew__hero">
           <img className="img-fluid" src={header}/>
+        </div>
+        <div className="container">
+          <div className="row">
+            <div className="col">
+              <Badge 
+                firstName="Paho"
+                lastName="Alapizco"
+                jobTitle="Project Manager día, Web Developer de noche"
+                twitter="pahoalapizco"
+              />
+            </div>
+          </div>
         </div>
       </div>
     )
