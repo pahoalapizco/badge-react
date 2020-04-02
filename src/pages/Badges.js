cb7723a7eeb907eefe4b3756e54dbee18b1955a8
Paola Alapizco <pahoc.alapizco@gmail.com>
2020-04-02 13:42:21 -0700

    Practice with the components life cycle

--- src/pages/Badges.js
+++ src/pages/Badges.js
@@ -6,37 +6,56 @@ import BadgesList from '../components/BadgesList';
 import './styles/Badges.css';
 
 class Badges extends React.Component {
-  state = {
-    data: [
-      {
-        "id": "98dd84a4-8dba-4a7d-87c2-3b9714372d2d",
-        "firstName": "Jed",
-        "lastName": "Sporer",
-        "email": "Santiago.Roberts@yahoo.com",
-        "jobTitle": "Dynamic Accounts Agent",
-        "twitter": "JedSporer30424",
-        "avatarUrl": "https://www.gravatar.com/avatar/4f164ea9a61e8d28e6b83a356d3ff5ae?d=identicon"
-      },
-      {
-        "firstName": "Richard",
-        "lastName": "Kaufman López",
-        "email": "richardbkaufman@gmail.com",
-        "avatarUrl": "https://www.gravatar.com/avatar/21594ed15d68ace3965642162f8d2e84?d=identicon",
-        "jobTitle": "Frontend Engineer @ Kickass Partners",
-        "twitter": "sparragus",
-        "id": "EYR2BVQ"
-      },
-      {
-        "firstName": "Carolynn",
-        "lastName": "Kaufman",
-        "email": "carolynn@example.com",
-        "avatarUrl": "https://www.gravatar.com/avatar/580a552f26ac5e3a0817c3ecc5b9d690?d=identicon",
-        "jobTitle": "Lawyer",
-        "twitter": "ckaufmanpr",
-        "id": "npaYZKI"
-      }
-    ]
-  };
+  
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: []
+    };
+  }
+  
+  componentDidMount(){
+    this.timeoutId = setTimeout(() => {
+      this.setState({
+        data: [
+          {
+            "id": "98dd84a4-8dba-4a7d-87c2-3b9714372d2d",
+            "firstName": "Jed",
+            "lastName": "Sporer",
+            "email": "Santiago.Roberts@yahoo.com",
+            "jobTitle": "Dynamic Accounts Agent",
+            "twitter": "JedSporer30424",
+            "avatarUrl": "https://www.gravatar.com/avatar/4f164ea9a61e8d28e6b83a356d3ff5ae?d=identicon"
+          },
+          {
+            "firstName": "Richard",
+            "lastName": "Kaufman López",
+            "email": "richardbkaufman@gmail.com",
+            "avatarUrl": "https://www.gravatar.com/avatar/21594ed15d68ace3965642162f8d2e84?d=identicon",
+            "jobTitle": "Frontend Engineer @ Kickass Partners",
+            "twitter": "sparragus",
+            "id": "EYR2BVQ"
+          },
+          {
+            "firstName": "Carolynn",
+            "lastName": "Kaufman",
+            "email": "carolynn@example.com",
+            "avatarUrl": "https://www.gravatar.com/avatar/580a552f26ac5e3a0817c3ecc5b9d690?d=identicon",
+            "jobTitle": "Lawyer",
+            "twitter": "ckaufmanpr",
+            "id": "npaYZKI"
+          }
+        ]
+      })
+    }, 3000);
+  }
+
+  componentDidUpdate(prevProps, prevState) {}
+
+  componentWillUnmount(){
+    clearTimeout(this.timeoutId);
+  }
   render() {
     return (
       <React.Fragment>
