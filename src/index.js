a76c95373f61858cbe5a0311842342a64e9feaa2
Paola Alapizco <pahoc.alapizco@gmail.com>
2020-04-01 19:22:07 -0700

    React router init

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.css';
 
+import App from './components/App';
 import BadgeNew from './pages/BadgeNew';
 import Badges from './pages/Badges';
 
@@ -9,5 +10,5 @@ const container = document.getElementById('app');
 
 // ReactDOM.render(QUE, DONDE); 
 ReactDOM.render(
-  <Badges />, 
+  <App />, 
   container);
